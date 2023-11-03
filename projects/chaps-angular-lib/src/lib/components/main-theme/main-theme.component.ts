d16c0a7d5e819e93603d59a9c4092e1ce714c824
JorgeChaparroS <jorgechaparrosarmiento@gmail.com>
2023-11-03 18:14:04 -0500

    Main theme

--- projects/chaps-angular-lib/src/lib/components/main-theme/main-theme.component.ts
+++ projects/chaps-angular-lib/src/lib/components/main-theme/main-theme.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'chaps-lib-main-theme',
   templateUrl: './main-theme.component.html',
-  styleUrls: ['./main-theme.component.scss']
+  styleUrls: ['./main-theme.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainThemeComponent {
 
